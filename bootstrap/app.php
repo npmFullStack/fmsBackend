e9ec89fd9280e6ac346dd22862b6ed1bf1fa42a7
npmFullStack <occ.mangorangca.norway@gmail.com>
2025-10-18 11:52:31 +0800

    added

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,12 +7,14 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
+        api: __DIR__.'/../routes/api.php', 
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
     )
-    ->withMiddleware(function (Middleware $middleware): void {
+    ->withMiddleware(function (\Illuminate\Foundation\Configuration\Middleware $middleware): void {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions): void {
+    ->withExceptions(function (\Illuminate\Foundation\Configuration\Exceptions $exceptions): void {
         //
-    })->create();
+    })
+    ->create();
